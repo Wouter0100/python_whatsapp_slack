f303fd299ba7527b043bfec651327e7285e84dd1
Wouter van Os <wouter0100@gmail.com>
2016-11-14 23:23:34 +0100

    Initial commit

---
+++ ChannelBridge.py
@@ -0,0 +1,233 @@
+import io
+from threading import Thread
+
+import time
+import yaml
+from _yaml import ScannerError
+
+from yowsup.layers import YowLayerEvent
+from yowsup.layers.auth import AuthError
+from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.network import YowNetworkLayer
+from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+from slackclient import SlackClient
+from yowsup.layers.protocol_profiles.protocolentities import SetStatusIqProtocolEntity
+from yowsup.stacks import YowStackBuilder
+
+
+def loadConfiguration():
+    loadedConfiguration = False
+
+    try:
+        f = open('config.yaml')
+        loadedConfiguration = yaml.safe_load(f)
+        f.close()
+    except ScannerError as e:
+        print(e)
+
+    return loadedConfiguration
+
+
+# Load configuration
+configuration = loadConfiguration()
+
+if not configuration:
+    print('Unable to load configuration')
+    exit(-1)
+
+# Initialize defaults
+credentials = (str(configuration['config']['whatsapp']['number']), configuration['config']['whatsapp']['password'])
+sc = SlackClient(configuration['config']['slack']['key'])
+
+
+# Channel bridge layer for Yowsup
+class ChannelBridgeLayer(YowInterfaceLayer):
+
+    def onConnected(self, yowLayerEvent):
+        super().onConnected(yowLayerEvent)
+
+        def onSuccess(resultIqEntity, originalIqEntity):
+            print('Successfully set sttatus')
+
+        def onError(errorIqEntity, originalIqEntity):
+            print('Error while setting status')
+
+        entity = SetStatusIqProtocolEntity('- Connects WhatsApp and Slack -')
+        self._sendIq(entity, onSuccess, onError)
+
+    def sendMessage(self, to, content):
+        outgoingMessage = TextMessageProtocolEntity(content, to=to)
+        self.toLower(outgoingMessage)
+
+    @ProtocolEntityCallback('message')
+    def onMessage(self, messageProtocolEntity):
+        postChannel = False
+
+        for channel, config in configuration['channels'].items():
+            if config['whatsapp'] == messageProtocolEntity.getFrom():
+                postChannel = config['slack']
+                break
+
+        if postChannel:
+            username = messageProtocolEntity.getAuthor(False)
+            icon = None
+
+            for contact, config in configuration['contacts'].items():
+                if str(config['whatsapp']) == str(messageProtocolEntity.getAuthor(False)):
+                    username = config['name']
+
+                    if 'slack' in config:
+                        profile = sc.api_call(
+                            'users.info',
+                            user=config['slack'],
+                        )
+
+                        icon = profile['user']['profile']['image_48']
+
+                    break
+
+            if messageProtocolEntity.getType() == 'text':
+                sc.api_call(
+                    'chat.postMessage',
+                    channel=postChannel,
+                    username=username,
+                    icon_url=icon,
+                    text=messageProtocolEntity.getBody()
+                )
+            elif messageProtocolEntity.getType() == 'media':
+                if messageProtocolEntity.getMediaType() == "image":
+                    file = sc.api_call(
+                        'files.upload',
+                        channels=postChannel,
+                        file=io.BytesIO(messageProtocolEntity.getMediaContent())
+                    )
+
+                    print(file)
+                else:
+                    print('Unsupported message media type passed through: ' + messageProtocolEntity.getType())
+            else:
+                print('Unsupported message type passed through: ' + messageProtocolEntity.getType())
+
+        else:
+            self.sendMessage(messageProtocolEntity.getFrom(),
+                             'Are you tokking to me? Ik ken dit gesprek niet.. Bel Wouter even!')
+
+        self.toLower(messageProtocolEntity.ack())
+        self.toLower(messageProtocolEntity.ack(True))
+
+    @ProtocolEntityCallback('receipt')
+    def onReceipt(self, entity):
+        self.toLower(entity.ack())
+
+
+channelBridgeLayer = ChannelBridgeLayer()
+
+
+def whatsapp():
+    while True:
+        try:
+
+            stackBuilder = YowStackBuilder()
+
+            stack = stackBuilder \
+                .pushDefaultLayers(True) \
+                .push(channelBridgeLayer) \
+                .build()
+
+            stack.setCredentials(credentials)
+            stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))
+
+            try:
+                stack.loop()
+            except AuthError as e:
+                print('Authentication Error: %s' % e.message)
+
+        except BaseException as e:
+            print(e)
+
+
+def slack():
+    while True:
+        try:
+            if sc.rtm_connect():
+                while True:
+                    messages = sc.rtm_read()
+
+                    for message in messages:
+                        print(message)
+
+                        if message['type'] == 'message':
+                            postChannel = False
+
+                            for channel, config in configuration['channels'].items():
+                                if config['slack'] == message['channel']:
+                                    postChannel = config['whatsapp']
+                                    break
+
+                            if postChannel:
+                                prefix = None
+
+                                if 'user' in message:
+                                    # Try to find user in contacts
+                                    for contact, config in configuration['contacts'].items():
+                                        if 'slack' not in config:
+                                            continue
+
+                                        if str(config['slack']) == str(message['user']):
+                                            prefix = config['name']
+
+                                    # Get firstname and lastname from Slack
+                                    if prefix is None:
+                                        user = sc.api_call(
+                                            'users.info',
+                                            user=message['user'],
+                                        )
+
+                                        if 'user' in user:
+                                            profile = user['user']['profile']
+                                            if 'firstname' in profile and 'last_name' in profile:
+                                                prefix = profile['real_name']
+
+                                            if 'username' in user['user']:
+                                                prefix = user['user']
+
+                                if prefix is None:
+                                    continue
+
+                                if 'subtype' in message:
+                                    if message['subtype'] == 'file_share':
+                                        channelBridgeLayer.sendMessage(postChannel, prefix + ' shared a file on Slack.')
+                                else:
+                                    channelBridgeLayer.sendMessage(postChannel, prefix + ': ' + message['text'])
+                            else:
+                                sc.api_call(
+                                    'chat.postMessage',
+                                    channel=message['channel'],
+                                    username='whatsapp',
+                                    text='Are you tokking to me? Ik ken dit gesprek niet.. Bel Wouter even!'
+                                )
+
+                    time.sleep(1)
+            else:
+                print('Connection Failed, invalid token?')
+        except BaseException as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    whatsappThread = Thread(target=whatsapp)
+    slackThread = Thread(target=slack)
+
+    whatsappThread.start()
+    slackThread.start()
+
+    while True:
+        time.sleep(10)
+
+        refreshConfiguration = loadConfiguration()
+
+        if not refreshConfiguration:
+            print('Unable to refresh configuration because of an error')
+            continue
+
+        configuration = refreshConfiguration
